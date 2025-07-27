0f016f7a99b85a68719ea47db816a7c48c3fc23c
Lorenzo Paredes <lorenzoparedes122@gmail.com>
2025-07-27 18:50:45 -0300

    Implementar botón para ver una compra en detalle

--- src/components/ListarVentas.jsx
+++ src/components/ListarVentas.jsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from 'react';
 import { listarVentas, actualizarEstadoCompra } from '../utils/api';
+import { useNavigate } from 'react-router-dom';
 
 const ListarVentas = () => {
     const [ventas, setVentas] = useState([]);
     const [busqueda, setBusqueda] = useState("");
     const [error, setError] = useState(null);
     const [cargandoId, setCargandoId] = useState(null);
+
+    const navigate = useNavigate();
 
     useEffect(() => {
         const fetchVentas = async () => {
@@ -58,6 +61,10 @@ const ListarVentas = () => {
         }
     };
 
+    const verDetallesCompra = (idCompra) => {
+        navigate(`/listar-ventas/${idCompra}`)
+    }
+
     const ventasFiltradas = ventas.filter((venta) =>
         venta.idCompra.toString().includes(busqueda)
     );
@@ -102,31 +109,50 @@ const ListarVentas = () => {
                     <td>${venta.total.toFixed(2)}</td>
                     <td>{venta.estado}</td>
                     <td>
-                      {venta.estado !== "ENTREGADO" && (
-                        <button
-                          className="btn-estado"
-                          onClick={() =>
-                            cambiarEstadoVenta(venta.idCompra, venta.estado)
-                          }
-                          disabled={cargandoId === venta.idCompra}
-                          title="Cambiar estado"
-                        >
-                          {cargandoId === venta.idCompra ? (
-                            <span>Cargando...</span>
-                          ) : (
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              width="20"
-                              height="20"
-                              fill="currentColor"
-                              viewBox="0 0 16 16"
-                              style={{ fill: "#555" }}
+                        <div style={{ display: 'flex', gap: '5px' }}>
+                            <button
+                                className="btn-detalles"
+                                onClick={() => verDetallesCompra(venta.idCompra)}
+                                title="Ver detalles"
                             >
-                              <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
-                            </svg>
-                          )}
-                        </button>
-                      )}
+                                <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="20"
+                                height="20"
+                                viewBox="0 0 16 16"
+                                style={{ fill: "#555" }}
+                                >
+                                <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
+                                <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
+                                </svg>
+                            </button>
+
+                            {venta.estado !== "ENTREGADO" && (
+                                <button
+                                    className="btn-estado"
+                                    onClick={() =>
+                                        cambiarEstadoVenta(venta.idCompra, venta.estado)
+                                    }
+                                    disabled={cargandoId === venta.idCompra}
+                                    title="Cambiar estado"
+                                >
+                                {cargandoId === venta.idCompra ? (
+                                    <span>Cargando...</span>
+                                ) : (
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="20"
+                                        height="20"
+                                        fill="currentColor"
+                                        viewBox="0 0 16 16"
+                                        style={{ fill: "#555" }}
+                                        >
+                                        <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
+                                    </svg>
+                                )}
+                                </button>
+                            )}
+                        </div>
                     </td>
                   </tr>
                 ))}
